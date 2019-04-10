b3b7ecbeae7df6b3e7d0b7c455e4a2b759f4f108
Ian <im337@drexel.edu>
2019-04-10 19:33:32 -0400

    updated aes.cpp

--- aes.cpp
+++ aes.cpp
@@ -1,15 +1,12 @@
 // AESEncrypt.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include "pch.h"
-#include <iostream>
 
 #include <iostream>
 #include <iomanip>
 
-#pragma comment(lib, "cryptlib.lib")
-
-#include "dll.h"
+#include <fstream>
+#include <string>
 
 #include "modes.h"
 #include "aes.h"
@@ -17,31 +14,34 @@
 
 int main(int argc, char* argv[]) {
 
-	//Key and IV setup
-	//AES encryption uses a secret key of a variable length (128-bit, 196-bit or 256-   
-	//bit). This key is secretly exchanged between two parties before communication   
-	//begins. DEFAULT_KEYLENGTH= 16 bytes
+
+	//Key String used for encryption
+	std::stringstream str;
+	std::string s1 = "encryptkey";
+	str << s1;
+	int value;
+	str >> std::hex >> value;
+
+	//declare keys
 	CryptoPP::byte key[CryptoPP::AES::DEFAULT_KEYLENGTH], iv[CryptoPP::AES::BLOCKSIZE];
-	memset(key, 0x00, CryptoPP::AES::DEFAULT_KEYLENGTH);
+	memset(key, value, CryptoPP::AES::DEFAULT_KEYLENGTH);
 	memset(iv, 0x00, CryptoPP::AES::BLOCKSIZE);
 
-	//
-	// String and Sink setup
-	//
-	std::string plaintext = "Now is the time for all good men to come to the aide...";
+	//set up plaintext string, and cipher and decrypted texts
+	std::string plaintext = "Example Text. This string will be a .json file.";
 	std::string ciphertext;
 	std::string decryptedtext;
 
-	//
+	// Dump Key
+	std::cout << "Key - " << s1 << std::endl;
+	std::cout << std::endl;
+
 	// Dump Plain Text
-	//
 	std::cout << "Plain Text (" << plaintext.size() << " bytes)" << std::endl;
 	std::cout << plaintext;
 	std::cout << std::endl << std::endl;
 
-	//
 	// Create Cipher Text
-	//
 	CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
 	CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption(aesEncryption, iv);
 
@@ -49,45 +49,51 @@ int main(int argc, char* argv[]) {
 	stfEncryptor.Put(reinterpret_cast<const unsigned char*>(plaintext.c_str()), plaintext.length());
 	stfEncryptor.MessageEnd();
 
-	//
 	// Dump Cipher Text
-	//
 	std::cout << "Cipher Text (" << ciphertext.size() << " bytes)" << std::endl;
 
 	for (int i = 0; i < ciphertext.size(); i++) {
-
 		std::cout << "0x" << std::hex << (0xFF & static_cast<CryptoPP::byte>(ciphertext[i])) << " ";
 	}
 
 	std::cout << std::endl << std::endl;
 
-	//
+	//write cipher text to file
+	std::ofstream encryptFile;
+	encryptFile.open("encryptedJSON.txt", std::ofstream::binary);
+	encryptFile << ciphertext;
+	encryptFile.close();
+
+	// Decrypt cipher text to new file
+	// read cipher text from encryptedJSON.txt to buffer
+	std::ifstream readEncrypt;
+	readEncrypt.open("encryptedJSON.txt", std::ofstream::binary);
+	std::stringstream buffer;
+	buffer << readEncrypt.rdbuf();
+	std::string cipherstr = buffer.str();
+
 	// Decrypt
-	//
 	CryptoPP::AES::Decryption aesDecryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
 	CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption(aesDecryption, iv);
 
 	CryptoPP::StreamTransformationFilter stfDecryptor(cbcDecryption, new CryptoPP::StringSink(decryptedtext));
-	stfDecryptor.Put(reinterpret_cast<const unsigned char*>(ciphertext.c_str()), ciphertext.size());
+//	stfDecryptor.Put(reinterpret_cast<const unsigned char*>(ciphertext.c_str()), ciphertext.size());
+	stfDecryptor.Put(reinterpret_cast<const unsigned char*>(cipherstr.c_str()), cipherstr.size());
 	stfDecryptor.MessageEnd();
 
-	//
+	//write to decryptedJSON.txt
+	std::ofstream decryptFile;
+	decryptFile.open("decryptedJSON.txt");
+	decryptFile << decryptedtext;
+	decryptFile.close();
+
 	// Dump Decrypted Text
-	//
 	std::cout << "Decrypted Text: " << std::endl;
 	std::cout << decryptedtext;
 	std::cout << std::endl << std::endl;
 
+//	system("pause");
+
 	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
